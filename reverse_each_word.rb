5d46261d5392fbdab988f62a0d34736ebd8b4edf
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-14 18:02:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
- def reverse_each_word(sentence)
-  sentence.reverse.split.reverse.join(" ")
+ def reverse_each_word(string)
+   string.each do |word|
+   word.reverse.split.reverse.join(" ")
  end
  
  
