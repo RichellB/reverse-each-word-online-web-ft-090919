bd4be211afcfc3fad8dfd564003a3a8842f827f1
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-14 17:41:34 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,23 @@
+def reverse_each_word(string)
+  reverse_array = []
+ my_array = string.split(",")
+ 
+ my_array.each do |word|
+   reverse_array << word.reverse!
+ end
+ reverse_array
+ end
+ 
+ def reverse_each_word(string)
+   
+ my_array = string.split(",")
+ 
+ my_array.collect do |word|
+   word.reverse
+ end
+ my_array
+ end 
+   
+   
+   
+ 
